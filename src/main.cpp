e05ec4ac25b4af13cfeb0dc232783e3d395e9d08
NathanW <nathansward@comcast.net>
2021-02-19 14:09:04 -0700

    initial setup commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿// Connor-Nathan-Game.cpp : Defines the entry point for the application.
+//
+
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
+#include <entt/entt.hpp>
+#include <SDL2/SDL.h>
+
+int main(int, char**)
+{
+	fmt::print("Hello {}!\n", "world");
+	return 0;
+}
