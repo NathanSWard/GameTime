a5f2d53a0e5281fbcc4d287d36c4b9abbdceb88e
NathanW <nathansward@comcast.net>
2021-02-25 18:11:43 -0700

    Move void_ptr to its own file

--- src/util/type_map.hpp
+++ src/util/type_map.hpp
@@ -6,81 +6,10 @@
 #include <type_traits>
 #include <unordered_map>
 
-namespace internal {
-
-    template <typename T>
-    requires (!std::is_trivially_destructible_v<T>)
-    constexpr void invoke_destructor(void* const ptr)
-    {
-        static_cast<T*>(ptr)->~T();
-    }
-
-    template <typename T>
-    requires (std::is_trivially_destructible_v<T>)
-    constexpr void invoke_destructor(void* const) {}
-
-    class void_ptr {
-        using deleter_t = void(*)(void*);
-        void* m_data = nullptr;
-        deleter_t m_deleter = nullptr;
-
-        constexpr void_ptr(void* const data, deleter_t const deleter) noexcept
-            : m_data(data)
-            , m_deleter(deleter)
-        {
-        }
-
-        constexpr void destory()
-        {
-            if (m_data) {
-                m_deleter(m_data);
-            }
-        }
-
-    public:
-        template <typename T, typename... Args>
-        static constexpr auto create(Args&&... args) noexcept -> void_ptr 
-        {
-            T* ptr = new T(std::forward<Args>(args)...);
-            return void_ptr(static_cast<void*>(ptr), invoke_destructor<T>);
-        }
-
-        constexpr void* take() noexcept 
-        {
-            auto const ptr = m_data;
-            m_data = nullptr;
-            m_deleter = nullptr;
-            return ptr;
-        }
-
-        constexpr auto data() noexcept -> void* { return m_data; }
-        constexpr auto data() const noexcept -> void const* { return m_data; }
-        constexpr auto cdata() const noexcept -> void const* { return m_data; }
-
-        constexpr void_ptr(void_ptr&& other) noexcept 
-            : m_data(std::exchange(other.m_data, nullptr))
-            , m_deleter(std::exchange(other.m_deleter, nullptr))
-        {
-        }
-
-        constexpr void_ptr& operator=(void_ptr&& other) noexcept
-        {
-            destory();
-            m_data = std::exchange(other.m_data, nullptr);
-            m_deleter = std::exchange(other.m_deleter, nullptr);
-            return *this;
-        }
-
-        void_ptr(void_ptr const&) = delete;
-        void_ptr& operator=(void_ptr const&) = delete;
-
-        ~void_ptr() { destory(); }
-    };
-
-} // namespace internal
+#include "void_ptr.hpp"
 
 class TypeMap {
-    std::unordered_map<type_id_t, internal::void_ptr> m_map;
+    std::unordered_map<type_id_t, void_ptr> m_map;
 
 public:
 
@@ -102,7 +31,7 @@ public:
     template <typename T, typename... Args>
     auto insert(Args&&... args) -> T&
     {
-        auto const [iter, ok] = m_map.insert_or_assign(type_id<T>(), internal::void_ptr::template create<T>(FWD(args)...));
+        auto const [iter, ok] = m_map.insert_or_assign(type_id<T>(), void_ptr::create<T>(FWD(args)...));
         UNUSED(ok);
         return *static_cast<T*>(iter->second.data());
     }
---
+++ src/util/void_ptr.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+namespace {
+
+    template <typename T>
+    requires (!std::is_trivially_destructible_v<T>)
+        constexpr void invoke_destructor(void* const ptr)
+    {
+        static_cast<T*>(ptr)->~T();
+    }
+
+    template <typename T>
+    requires (std::is_trivially_destructible_v<T>)
+        constexpr void invoke_destructor(void* const) {}
+
+} // namespace
+
+class void_ptr {
+    using deleter_t = void(*)(void*);
+    void* m_data = nullptr;
+    deleter_t m_deleter = nullptr;
+
+    constexpr void_ptr(void* const data, deleter_t const deleter) noexcept
+        : m_data(data)
+        , m_deleter(deleter)
+    {
+    }
+
+    constexpr void destory()
+    {
+        if (m_data) {
+            m_deleter(m_data);
+        }
+    }
+
+public:
+    template <typename T, typename... Args>
+    static constexpr auto create(Args&&... args) noexcept -> void_ptr
+    {
+        T* ptr = new T(std::forward<Args>(args)...);
+        return void_ptr(static_cast<void*>(ptr), invoke_destructor<T>);
+    }
+
+    constexpr void* take() noexcept
+    {
+        auto const ptr = m_data;
+        m_data = nullptr;
+        m_deleter = nullptr;
+        return ptr;
+    }
+
+    constexpr auto data() noexcept -> void* { return m_data; }
+    constexpr auto data() const noexcept -> void const* { return m_data; }
+    constexpr auto cdata() const noexcept -> void const* { return m_data; }
+
+    constexpr void_ptr(void_ptr&& other) noexcept
+        : m_data(std::exchange(other.m_data, nullptr))
+        , m_deleter(std::exchange(other.m_deleter, nullptr))
+    {
+    }
+
+    constexpr void_ptr& operator=(void_ptr&& other) noexcept
+    {
+        destory();
+        m_data = std::exchange(other.m_data, nullptr);
+        m_deleter = std::exchange(other.m_deleter, nullptr);
+        return *this;
+    }
+
+    void_ptr(void_ptr const&) = delete;
+    void_ptr& operator=(void_ptr const&) = delete;
+
+    ~void_ptr() { destory(); }
+};
