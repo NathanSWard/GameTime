de7dc28ccf19c017e5e01026a351232b6f7f8c1e
NathanSWard <nathansward@comcast.net>
2021-03-19 13:49:03 -0600

    Disable entt's signal handlers for components add/mod/del

--- src/core/ecs/world.hpp
+++ src/core/ecs/world.hpp
@@ -2,6 +2,12 @@
 
 #include <entt/entt.hpp>
 
+template<typename Entity, typename Type>
+struct entt::storage_traits<Entity, Type> {
+    // prevents entt:: from using its internal signals.
+    using storage_type = storage_adapter_mixin<basic_storage<Entity, Type>>;
+};
+
 using World = entt::registry;
 
 using entity_t = entt::entity;
